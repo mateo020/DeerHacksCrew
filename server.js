8307553a4bf9b0a8b15679922ccdce8e662b7747
xela <perocho.xela@gmail.com>
2024-02-18 08:01:13 -0500

    openai and overpass

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+
 const PORT = 8000;
-const API_KEY = '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';
+const API_KEY = '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';
 const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post('/interpreter', async (req, res) => {
+    console.log(req.query);
+    const overpassOptions = {
+        method: "POST",
+        body: "data=" + encodeURIComponent(`[out:json];
+(
+  node["amenity"="university"](area:Canada);
+  way["amenity"="university"](area:Canada);
+  relation["amenity"="university"](area:Canada);
+);
+out body;
+>;
+out skel qt;`)//${res.query}
+    };
+    var overpassResult = await fetch(
+        "https://overpass-api.de/api/interpreter", overpassOptions
+    ).then(
+        (data) => data.json()
+    )
+    console.log(JSON.stringify(overpassResult, null, 2))
+});
 
 app.post('/completions', async (req, res) => {
     const options = {
@@ -23,15 +46,18 @@ app.post('/completions', async (req, res) => {
         })
     }
     try {
-        console.log("Attempting to fetch response from openai");
-        const APIResponse = await fetch("https://api.openai.com/v1/chat/completions", options);
-        const data = await APIResponse.json();
-        res.send(data);
+        console.log("(SS) Attempting to fetch data from OpenAI");
+        console.log("(SS) CS input: " + req.body.message + " CS input end");
+        const OpenAIResponse = await fetch("https://api.openai.com/v1/chat/completions", options);
+        console.log("(SS) OpenAIResponse: " + OpenAIResponse);
+        const OpenAIData = await OpenAIResponse.json();
+        console.log("(SS) OpenAIData: " + OpenAIData);
+        res.send(OpenAIData);
     } catch (error) {
         console.error(error)
     }
 });
+
 app.listen(PORT, () => {
     console.log("running on port " + PORT);
 });
-
--- src/Pages/Home/TerraTalk.jsx
+++ src/Pages/Home/TerraTalk.jsx
@@ -2,34 +2,57 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+
+
 const TerraTalk = (props) => {
     const preface = 'Generate an Overpass-QL Query that displays the following prompt: "I am looking for ';
     const postface = '" Only generate the Query Language Code. Do not add any comments.';
-
     const [userIn, setUserIn] = useState('');
     const [overpassQuery, setOverpassQuery] = useState('');
-    const getOverpassQuery = async () => {
+    const getOverpassResponse = async (req, res) => {
+        const options = {
+            method: "POST",
+            query: overpassQuery
+        }
+        try {
+            console.log("(CS) Attempting to retrieve response from Overpass");
+            const overpassResponse = await fetch("http://localhost:8000/interpreter", options);
+            console.log("(CS) Ocerpass Response: " + overpassResponse);
+            const overpassData = await overpassResponse.json();
+            console.log("(CS) Overpass Data: " + overpassData + postface);
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    const getOpenAIResponse = async () => {
         const options = {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                message: `${preface + userIn + postface}`
+                message: `${preface + userIn}`
             })
         }
         try {
             console.log("Attempting to retrieve response from GPT-3");
-            const serverResponse = await fetch("http://localhost:8000/completions", options);
-            const data = await serverResponse.json();
-            console.log(data);
-            setOverpassQuery(data.choices[0].message.content);
-            console.log(overpassQuery);
+            const openAIResponse = await fetch("http://localhost:8000/completions", options);
+            const openAIData = await openAIResponse.json();
+            console.log(openAIData);
+            setOverpassQuery(openAIData.choices[0].message.content);
         }
         catch (error) {
             console.error(error)
         }
     }
+
+    useEffect(() => {
+        console.log("Query Updated:\n" + overpassQuery);
+        getOverpassResponse();
+    }, [overpassQuery]);
+
 
     return (
         <section id="terraTalk" className="terra">
@@ -46,15 +69,15 @@ const TerraTalk = (props) => {
                     </label>
 
                 </div>
-                <button className="btn btn-primary" onClick={() => { getOverpassQuery() }}>Search</button>
+                <button className="btn btn-primary" onClick={() => { getOpenAIResponse() }}>Search</button>
             </div>
             <div className='map--box'>
-            <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </MapContainer>
+                <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </MapContainer>
             </div>
         </section>
     )
