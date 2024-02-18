1da11194452daf465ef4ffe993ee5152e9d66308
Jennifer Cao <jennifer.cao.ca@gmail.com>
2024-02-18 07:24:25 -0500

    styled map page

--- src/Pages/Home/TerraTalk.jsx
+++ src/Pages/Home/TerraTalk.jsx
@@ -48,12 +48,14 @@ const TerraTalk = (props) => {
                 </div>
                 <button className="btn btn-primary" onClick={() => { getOverpassQuery() }}>Search</button>
             </div>
+            <div className='map--box'>
             <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
             </MapContainer>
+            </div>
         </section>
     )
 }
