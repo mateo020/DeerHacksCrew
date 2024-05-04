b393143619b1e417f52172012e0a95c41d1b6ffc
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-05-04 14:58:03 -0400

    added testing file to plot test markers

--- terratalk/src/components/Map.tsx
+++ terratalk/src/components/Map.tsx
@@ -1,33 +1,48 @@
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
-import L from 'leaflet';
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import L from "leaflet";
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+import overpassResult from "@/test/overpassResult.json";
+
+const elements = overpassResult.elements;
 
 let DefaultIcon = L.icon({
     iconUrl: icon.src,
-    shadowUrl: iconShadow.src
+    shadowUrl: iconShadow.src,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-const position: L.LatLngExpression | undefined = [51.505, -0.09]
+const position: L.LatLngExpression | undefined = [51.505, -0.09];
 
 function Map() {
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "500px", marginRight: "2rem" }}>
+        <MapContainer
+            center={position}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "500px", marginRight: "2rem" }}
+        >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]} >
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
-    )
-}
 
+            {elements.map((element) => {
+                if (element.lat && element.lon && element.tags) {
+                    return (
+                        <Marker position={[element.lat, element.lon]} key={element.id}>
+                            <Popup>
+                                {element.tags?.name} <br /> {element.tags?.leisure}.
+                            </Popup>
+                        </Marker>
+                    );
+                }
+            })}
+        </MapContainer>
+    );
+}
 
 export default Map;
